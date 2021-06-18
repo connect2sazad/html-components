643b7eeaf290aac47f7c74919d183318dcebe932
Sazad Ahemad <60960500+connect2sazad@users.noreply.github.com>
2021-06-18 22:27:04 +0530

    added logo

--- index.php
+++ index.php
@@ -12,6 +12,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        html, body{
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+        }
+    </style>
     <link rel="stylesheet" href="<?php echo HTML_COMPONENT; ?>style.css">
 </head>
 <body>
