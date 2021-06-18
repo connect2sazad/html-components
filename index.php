622651c1d09005a882d28488d04adc21b33e6c6f
Sazad Ahemad <60960500+connect2sazad@users.noreply.github.com>
2021-06-18 21:57:39 +0530

    added header1 component

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    define("COMPONENT", 'header1');
+    define("HTML_COMPONENT", COMPONENT."/");
+    define("INDEX", COMPONENT."/index.html");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo HTML_COMPONENT; ?>style.css">
+</head>
+<body>
+    <?php include_once(INDEX) ?>
+</body>
+</html>
